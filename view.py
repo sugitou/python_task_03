606134b1beaa99fec60dd150f9e7d0b682583d69
sugitou <sugi_tou108@yahoo.co.jp>
2021-07-21 23:00:41 +0900

    python戻り値を取得してログ出力

--- view.py
+++ view.py
@@ -2,20 +2,18 @@ import eel
 import desktop
 import search
 
-app_name="html"
+app_name="web"
 end_point="index.html"
 size=(700,600)
 
 text = ''
 
+
 @ eel.expose
 def kimetsu_search(word, csv_file):
     search_result = search.kimetsu_search(word, csv_file)
-    text = search_result
-    print('text', text)
+    return search_result
 
-eel.init("web")
-eel.view_log_js(text)
-eel.start("index.html")
+
 desktop.start(app_name,end_point,size)
 #desktop.start(size=size,appName=app_name,endPoint=end_point)
